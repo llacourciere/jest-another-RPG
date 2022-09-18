8f90b2db8290e86f48a05b36bde9bf8e649e6631
LaLacou <108777796+users.noreply.github.com>
2022-09-17 17:34:05 -0700

    created player inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,8 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
 // require the player module
 const Player = require('../lib/Player');
 
@@ -8,4 +13,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
